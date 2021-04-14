b3f0b2402054d28586e3ef3dfc04ef49ba87669f
Nguyễn Công Toàn <tn.toannguyen2811@gmail.com>
2021-04-14 14:41:41 +0700

    first commit

---
+++ src/main/java/vn/com/nhomtruyen/config/DatabaseConfiguration.java
@@ -0,0 +1,16 @@
+package vn.com.nhomtruyen.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import tech.jhipster.config.JHipsterConstants;
+
+@Configuration
+@EnableJpaRepositories("vn.com.nhomtruyen.repository")
+@EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAware")
+@EnableTransactionManagement
+public class DatabaseConfiguration {}
---
+++ src/test/java/vn/com/nhomtruyen/IntegrationTest.java
@@ -0,0 +1,17 @@
+package vn.com.nhomtruyen;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import vn.com.nhomtruyen.TruyenServerApp;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = TruyenServerApp.class)
+public @interface IntegrationTest {
+}
